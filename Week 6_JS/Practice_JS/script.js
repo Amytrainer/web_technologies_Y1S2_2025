8f99e97f77479479b350131da39915fba979ba9d
NKT-2634 <122818549+NKT-2634@users.noreply.github.com>
2025-03-21 16:08:23 +0000

    updated

---
+++ Week 6_JS/Practice_JS/script.js
@@ -0,0 +1,41 @@
+// 1.Variables
+// let cat="Rex";// The is what called initialisation of variable and giving it value
+// console.log(cat);
+
+//Datatypes in programming
+// int
+// string
+// Array float bool
+
+// 3.Loops and decisions
+// let age = 100; //declare variable
+
+// if (age < 14) {
+//   //Operator
+//   alert("Child's Ticket");
+// } else if (age < 65) {
+//   alert("Adult Ticket");
+// } else {
+//   alert("Travel Free");
+// }
+
+// Quiz: Create a program to showcase if its raining or not. And alert me its raining to take my umbrella 
+
+// let isRaining =true;//Camel case 
+
+// // Reuse Function Method
+// function checkWeather(isRaining){
+//     if(isRaining){
+//         alert("It's raining-dont forget your umbrella");
+    
+//     }
+//     else{
+//         alert("Oh hoo you forget-Start eating Almonds");
+//     }
+
+// }
+// //Test the function
+// checkWeather(true);
+// checkWeather(false);
+
+document.getElementById("example").style.background="brown";
